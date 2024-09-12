911d7b47f0602876f17d90df25175bed15ab4ef4
TejaswiniShukla <TejaswiniShukla11478@gmail.com>
2024-09-12 14:56:30 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import { Box } from "@mui/material";
+import React from "react";
+import "./App.css";
+
+function App() {
+  const bricks = [
+    { width: "450px", height: "200px", backgroundColor: "green" },
+    { width: "450px", height: "200px", backgroundColor: "yellow" },
+    { width: "450px", height: "200px", backgroundColor: "pink" },
+    { width: "450px", height: "200px", backgroundColor: "orange" },
+    { width: "450px", height: "200px", backgroundColor: "white" },
+    { width: "450px", height: "200px", backgroundColor: "purple" },
+  ];
+  return (
+    <>
+      <Box
+        className="logos"
+        sx={{
+          mt: 5,
+        }}
+      >
+        <Box className="logos-slide">
+          {bricks.map((e) => {
+            return (
+              <Box
+                sx={{
+                  width: e.width,
+                  height: e.height,
+                  backgroundColor: e.backgroundColor,
+                }}
+              ></Box>
+            );
+          })}
+        </Box>
+        <Box className="logos-slide">
+          {bricks.map((e) => {
+            return (
+              <Box
+                sx={{
+                  width: e.width,
+                  height: e.height,
+                  backgroundColor: e.backgroundColor,
+                }}
+              ></Box>
+            );
+          })}
+        </Box>
+        <Box className="logos-slide">
+          {bricks.map((e) => {
+            return (
+              <Box
+                sx={{
+                  width: e.width,
+                  height: e.height,
+                  backgroundColor: e.backgroundColor,
+                }}
+              ></Box>
+            );
+          })}
+        </Box>
+      </Box>
+      <Box
+        className="logos"
+        sx={{
+          mt: 5,
+        }}
+      >
+        <Box className="logos-slide2">
+          {bricks.map((e) => {
+            return (
+              <Box
+                sx={{
+                  width: e.width,
+                  height: e.height,
+                  backgroundColor: e.backgroundColor,
+                }}
+              ></Box>
+            );
+          })}
+        </Box>
+        <Box className="logos-slide2">
+          {bricks.map((e) => {
+            return (
+              <Box
+                sx={{
+                  width: e.width,
+                  height: e.height,
+                  backgroundColor: e.backgroundColor,
+                }}
+              ></Box>
+            );
+          })}
+        </Box>
+        <Box className="logos-slide2">
+          {bricks.map((e) => {
+            return (
+              <Box
+                sx={{
+                  width: e.width,
+                  height: e.height,
+                  backgroundColor: e.backgroundColor,
+                }}
+              ></Box>
+            );
+          })}
+        </Box>
+      </Box>
+    </>
+  );
+}
+export default App;
